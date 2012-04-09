ce201fbe441721eb0997d83abe7307a72656af40
Vasudev Kamath <kamathvasudev@gmail.com>
2012-04-09 22:39:32 +0530

    More comments

--- silpa.py
+++ silpa.py
@@ -1,34 +1,7 @@
 from flask import Flask, request, g, redirect, url_for,\
     abort, render_template, flash
-
-import sys
 import constants
-from constants import modules,modulenames,MODULES
-
-
-def load_modules():
-    '''
-      Load the modules enabled in the configuration file
-      by user. This function initializes global variable
-      MODULES which is a dictionary having module name as
-      key and module itself as value. Which can be used later
-      to process requests coming for the modules.
-    '''
-
-    for key in modules.keys():
-        if modules.get(key) == 'yes':
-            mod = None
-            try:
-                mod = sys.modules[key]
-                if not type(mod).__name__ == 'module':
-                    raise KeyError
-            except KeyError:
-                mod = __import__(key,globals(),locals(),[])
-                mod = getattr(mod,key)
-                sys.modules[key] = mod.getInstance()
-            if mod:
-                MODULES[key] = mod.getInstance()
-
+from constants import modules,modulenames
 
 app = Flask(__name__)
 app.config.from_object(constants.__name__)
@@ -36,20 +9,40 @@ app.config.from_object(constants.__name__)
 
 @app.route('/Soundex')
 def Soundex():
+    '''
+      This function is responsible to handle the /Soundex module page request.
+      It just returns the rendered template
+    '''
     return render_template('soundex.html',title='Soundex Module',modules = (modulenames[x] for x  in modules.keys() if modules[x] == "yes"))
 
 @app.route('/ApproxSearch')
 def ApproxSearch():
+    '''
+      This function is responsible to handle the /ApproxSearch module page request.
+      It just returns the rendered template
+    '''
     return render_template('approxsearch.html',title='ApproxSearch Module',modules = (modulenames[x] for x  in modules.keys() if modules[x] == "yes"))
 
 @app.route('/JSONRPC')
 def JSONRPC():
+    '''
+     This module is responsible for handling module processing request which is implemented
+     as JSONRPC.
+
+     #TODO: Implement it :P
+    '''
     pass
 
 @app.route('/')
 def main_page():
-    return render_template('index.html',title='Indic Computing Platform',modules=(modulenames[x] for x in modules.keys() if modules[x] == "yes"))
+    '''
+     This function renders the main page of Silpa
+    '''
+    return render_template('index.html',title='SILPA',modules=(modulenames[x] for x in modules.keys() if modules[x] == "yes"))
+
 
 if __name__ == '__main__':
     app.debug = True
+    # Lets intialized enabled module before starting server
+    constants.load_modules()
     app.run()
