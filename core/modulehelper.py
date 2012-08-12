e7e745bf1dc4cef949b7d4ce776e0ef2826c7c6c
Jishnu <jishnu7@gmail.com>
2012-08-12 23:40:11 +0530

    PEP8 cleaning

--- core/modulehelper.py
+++ core/modulehelper.py
@@ -1,9 +1,11 @@
 '''
   Purpose of this file is to hold variables which is used
-  across the application so that these variables don't get initialized multiple times
+  across the application so that these variables don't get
+  initialized multiple times
 '''
 
-_all_ = ['MODULES', 'modules', 'modulenames', 'enabled_modules', 'load_modules', 'BASEURL']
+_all_ = ['MODULES', 'modules', 'modulenames', 'enabled_modules', \
+        'load_modules', 'BASEURL']
 
 import loadconfig
 import sys
@@ -15,7 +17,9 @@ BASEURL = loadconfig.get('baseurl')
 modules = loadconfig.get('modules')
 modulenames = loadconfig.get('modules_display')
 
-enabled_modules = [modulenames[x] for x in modules.keys() if modules[x] == "yes"]
+enabled_modules = [modulenames[x] for x in modules.keys() \
+        if modules[x] == "yes"]
+
 
 def load_modules():
     '''
@@ -29,7 +33,7 @@ def load_modules():
     # Already initialized the modules then don't do it again
     if len(MODULES) != 0:
         return
-    
+
     for key in modules.keys():
         if modules.get(key) == 'yes':
             mod = None
@@ -39,13 +43,14 @@ def load_modules():
                     raise KeyError
             except KeyError:
                 try:
-                    mod = __import__(key,globals(),locals(),[])
+                    mod = __import__(key, globals(), locals(), [])
                     sys.modules[key] = mod
                 except ImportError:
                     # Since we can't use logger from flask as its not yet
                     # activated we will write it to sys.stderr this should
                     # go to webserver error.log
-                    print >> sys.stderr, "Failed to import module {0}".format(key)
+                    print >> sys.stderr, "Failed to import module {0}"\
+                            .format(key)
                     pass
             if mod:
                 MODULES[key] = mod.getInstance()
--- loadconfig.py
+++ loadconfig.py
@@ -3,33 +3,34 @@ import os
 
 _all_ = ['get']
 
+
 class _SilpaConfig:
     def __init__(self):
         _config = RawConfigParser()
-        _config.read(os.path.join(os.path.dirname(__file__),'silpa.conf'))
+        _config.read(os.path.join(os.path.dirname(__file__), 'silpa.conf'))
 
-        self.site_name = _config.get('main','site')
-        self.baseurl = _config.get('main','baseurl')
+        self.site_name = _config.get('main', 'site')
+        self.baseurl = _config.get('main', 'baseurl')
 
-        self.log_level = _config.get('logging','log_level')
-        
-        folder = _config.get('logging','log_folder')
+        self.log_level = _config.get('logging', 'log_level')
+
+        folder = _config.get('logging', 'log_folder')
         self.log_folder = folder if folder != "." else os.getcwd()
 
-        name = _config.get('logging','log_name')
+        name = _config.get('logging', 'log_name')
         self.log_name = name if name else 'silpa.log'
 
         self.modules = {}
-        for module,status in _config.items('modules'):
+        for module, status in _config.items('modules'):
             self.modules[module] = status if status else "no"
 
         self.modules_display = {}
-        for module,name in _config.items('module_display'):
+        for module, name in _config.items('module_display'):
             self.modules_display[module] = name
+
 
 _config = _SilpaConfig()
 
+
 def get(key):
     return _config.__dict__[key]
-        
-        
