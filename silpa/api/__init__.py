0f32cc35b4094af6b22af52c2916e3979123f070
Vasudev Kamath <kamathvasudev@gmail.com>
2014-04-12 20:04:15 +0530

    Wrie app factory and fix http return code
    introduce create_app function as factory function, and on error in
    jsonrpc call return 400 error code instead of 200.
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- silpa/api/__init__.py
+++ silpa/api/__init__.py
@@ -1,5 +1,8 @@
 from flask import jsonify
 from functools import wraps
+from .. import factory
+
+import os
 
 
 def route(bp, *args, **kwargs):
@@ -7,8 +10,18 @@ def route(bp, *args, **kwargs):
         @bp.route(*args, **kwargs)
         @wraps(f)
         def wrapper(*args, **kwargs):
-            sc = 200
             result = f(*args, **kwargs)
+            sc = 200 if 'error' not in result else 400
             return jsonify(result), sc
         return f
     return decorator
+
+
+def create_app(settings_override=None):
+    app = factory.create_app(__name__, os.path.dirname(__file__),
+                             settings_override)
+    app.errorhandler(404)(on_404)
+
+
+def on_404(e):
+    return jsonify(dict(error='Not Found')), 404
