f0b4f6717d72433040f8ada75fb8404bad6edb3f
Vasudev Kamath <kamathvasudev@gmail.com>
2014-04-12 13:31:11 +0530

    Add method invoking logic to JSON-RPC
    Logic for invoking requested method on a module is added, if module
    doesn't have a method by the requested name a METHOD_NOT_FOUND error is
    returned to client as per JSON-RPC 2.0 specifications.
    
    If there is error in execution of call, INTERNAL_ERROR is returned to
    client as per JSON-RPC spec and message is set appropriately to reflect
    what happened.
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- silpa/api/jsonrpc.py
+++ silpa/api/jsonrpc.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 from flask import Blueprint, request
 from . import route
 import json
+import sys
 
 JsonRpcError = namedtuple('JsonRpcError', ['code', 'message', 'data'])
 JsonRpcRequest = namedtuple('JsonRpcRequest',
@@ -29,11 +30,21 @@ def handle_jsonrpc_call():
         if rpc_object.error_response is not None:
             # There was a error, translate and return the dictionary
             # object for client
-            return dict(zip(rpc_object.error_response._fields))
+            return dict(zip(rpc_object.error_response._fields,
+                            rpc_object.error_response))
         else:
             # there was no problem constructing request lets process
             # the call
-            pass
+            try:
+                rpc_object()
+            except Exception as e:
+                # Possible errors in execution of method
+                error = JsonRpcError(code=_INTERNAL_ERROR, message=e.message,
+                                     data=dict(zip(rpc_object.request._fields,
+                                                   rpc_object.request)))
+                return dict(jsonrpc="2.0",
+                            error=dict(zip(error._fields, error),
+                                       id=rpc_object.request.id))
 
 
 class JsonRpc(object):
@@ -62,4 +73,27 @@ class JsonRpc(object):
 
     def __call__(self):
         # process request here
-        pass
+        module, method = self.request.method.split('.')
+
+        if module not in sys.modules:
+            # Module is not yet loaded? handle this
+            pass
+        else:
+            # module is present in sys
+            mod = sys.modules[module]
+            if not hasattr(mod, method) and \
+               type(getattr(mod, method).__name__ == 'function'):
+                # method not found
+                error = JsonRpcError(code=_METHOD_NOT_FOUND,
+                                     message="requested method not found",
+                                     data="Requested method {}".format(
+                                         self.request.method))
+                self.error_response = JsonRpcErrorResponse(jsonrpc="2.0",
+                                                           error=error,
+                                                           id=self.request.id)
+            else:
+                # Method present so lets call it to get result
+                result = getattr(module, method)(*self.request.params)
+                self.response = JsonRpcResultResponse(jsonrpc="2.0",
+                                                      result=result,
+                                                      id=self.request.id)
