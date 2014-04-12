80c00aa1f31fe740577158ac2668870388711d61
Vasudev Kamath <kamathvasudev@gmail.com>
2014-04-12 11:29:51 +0530

    improve modulehelper module
    Use importlib module to import modules, these modules will be accessible
    to applications from sys.modules so drop local caching of modules.
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- silpa/modulehelper.py
+++ silpa/modulehelper.py
@@ -4,52 +4,19 @@
   initialized multiple times
 '''
 
-_all_ = ['MODULES', 'modules', 'modulenames', 'enabled_modules', \
-        'load_modules', 'BASEURL']
+from loadconfig import config
+from flask import globals
 
-import loadconfig
-import sys
+import importlib
 
-
-MODULES = {}
-BASEURL = loadconfig.get('baseurl')
-
-modules = loadconfig.get('modules')
-modulenames = loadconfig.get('modules_display')
-
-enabled_modules = [modulenames[x] for x in modules.keys() \
-        if modules[x] == "yes"]
+_modules = [module for module, need in config.items("modules")
+            if need == "yes"]
 
 
 def load_modules():
-    '''
-      Load the modules enabled in the configuration file
-      by user. This function initializes global variable
-      MODULES which is a dictionary having module name as
-      key and module itself as value. Which can be used later
-      to process requests coming for the modules.
-    '''
-
-    # Already initialized the modules then don't do it again
-    if len(MODULES) != 0:
-        return
-
-    for key in modules.keys():
-        if modules.get(key) == 'yes':
-            mod = None
-            try:
-                mod = sys.modules[key]
-                if not type(mod).__name__ == 'module':
-                    raise KeyError
-            except KeyError:
-                try:
-                    mod = __import__(key, globals(), locals(), [])
-                except ImportError:
-                    # Since we can't use logger from flask as its not yet
-                    # activated we will write it to sys.stderr this should
-                    # go to webserver error.log
-                    print >> sys.stderr, "Failed to import module {0}"\
-                            .format(key)
-                    pass
-            if mod:
-                MODULES[key] = mod.getInstance()
+    app = globals.current_app
+    for module in _modules:
+        try:
+            importlib.import_module(module)
+        except ImportError:
+            app.logger.error("Failed to import {module}".format(module=module))
