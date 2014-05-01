865478534188bda43db6a7908f6097d373c28d09
Vasudev Kamath <kamathvasudev@gmail.com>
2014-05-01 12:04:16 +0530

    Missed rewriting logic from  commit 58678bc
    The explanation for this commit was included in commit 58678bc but some
    how it didn't get included in that commit, now fixed.
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- silpa/frontend/__init__.py
+++ silpa/frontend/__init__.py
@@ -1,5 +1,4 @@
 from .. import factory
-from functools import wraps
 from jinja2 import PackageLoader, ChoiceLoader
 from ..loadconfig import config
 
@@ -18,20 +17,3 @@ def load_module_templates(app):
     for module in modules:
         templates.append(PackageLoader(module))
     app.jinja_loader = ChoiceLoader(templates)
-
-
-def route(bp, *args, **kwargs):
-    def decorator(f):
-        # FIXME: when we drop python2 support use unpack feature from
-        # python3
-        baseurl, module_display = args[0], args[1]
-        for module in module_display:
-            bp.add_url_rule(baseurl + module, view_func=f)
-
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            print(args)
-            print(kwargs)
-            return f(*args, **kwargs)
-        return f
-    return decorator
--- silpa/frontend/pages.py
+++ silpa/frontend/pages.py
@@ -1,6 +1,5 @@
-from flask import request, Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort
 from ..loadconfig import config
-from . import route
 
 _BASE_URL = config.get('main', 'baseurl')
 _modules = [module for module, need in config.items('modules')
@@ -16,37 +15,31 @@ _display_module_map = {display_name: module for module, display_name in
 bp = Blueprint('frontend', __name__)
 
 
-@bp.route(_BASE_URL)
-@bp.route(_BASE_URL + 'index.html')
-@bp.route(_BASE_URL + 'License')
-@bp.route(_BASE_URL + 'Credits')
-@bp.route(_BASE_URL + 'Contact')
-def serve_pages():
-    if request.path == _BASE_URL:
+@bp.route(_BASE_URL, defaults={'page': 'index.html'})
+@bp.route(_BASE_URL + '<page>')
+def serve_pages(page):
+    if page == "index.html":
         return render_template('index.html', title='SILPA',
                                main_page=_BASE_URL,
                                modules=_modulename_to_display)
-    elif request.path == _BASE_URL + "License":
+    elif page == "License":
         return render_template('license.html', title='SILPA License',
                                main_page=_BASE_URL,
                                modules=_modulename_to_display)
-    elif request.path == _BASE_URL + "Credits":
+    elif page == "Credits":
         return render_template('credits.html', title='Credits',
                                main_page=_BASE_URL,
                                modules=_modulename_to_display)
-    elif request.path == _BASE_URL + "Contact":
+    elif page == "Contact":
         return render_template('contact.html', title='Contact SILPA Team',
                                main_page=_BASE_URL,
                                modules=_modulename_to_display)
-
-
-@route(bp, _BASE_URL, _modulename_to_display)
-def serve_module_page():
-    request_mod = request.path.split('/')[-1]
-    if request_mod in _display_module_map:
-        return render_template(_display_module_map[request_mod] + '.html',
-                               title=request_mod, main_page=_BASE_URL,
-                               modules=_modulename_to_display)
     else:
-        # Did we encounter something which is not registered by us?
-        return abort(404)
+        # modules requested!.
+        if page in _display_module_map:
+            return render_template(_display_module_map[page] + '.html',
+                                   title=page, main_page=_BASE_URL,
+                                   modules=_modulename_to_display)
+        else:
+            # Did we encounter something which is not registered by us?
+            return abort(404)
