ba0697efee789429c51e9fe5b5788d386dcb94e3
Vasudev Kamath <kamathvasudev@gmail.com>
2014-05-01 11:52:07 +0530

    Enable tests for all pages and modules

--- tests/frontend/mainpage_tests.py
+++ tests/frontend/mainpage_tests.py
@@ -15,3 +15,35 @@ class MainPageTestCase(SILPAFrontEndTestCase):
         r = self.get('/')
         self.assertIn('<title> SILPA - Indic Language Computing Platform ' +
                       '</title>', self.assertOk(r).data)
+
+    def test_pages(self):
+        r = self.get('/License')
+        self.assertIn('<h1>License</h1>', self.assertOk(r).data)
+
+        r = self.get('/Contact')
+        self.assertIn('<h1>Contacts</h1>', self.assertOk(r).data)
+
+        r = self.get('/Credits')
+        self.assertIn('<h1>Credits</h1>', self.assertOk(r).data)
+
+    def test_moduleloaded(self):
+        from silpa.loadconfig import config
+        modules = [module for module, need in config.items('modules')
+                   if need == "yes"]
+        module_display = sorted((display_name
+                                 for module, display_name in
+                                 config.items('module_display')
+                                 if module in modules))
+        r = self.get('/')
+        self.assertOk(r)
+
+        for m in module_display:
+            # FIXME: Returned by configparser is unicode string for
+            # some reason breaks with assertIn not sure why :(
+            self.assertIn(m.encode('utf-8'), r.data)
+
+            # TODO: URL in modules needs to be fixed befor enabling
+            # below tests
+            # r1 = self.get('/' + m)
+            # self.assertIn('<title> {} - Indic Language Computing Platform ' +
+            #               '</title>', self.assertOk(r1).data)
