c1d95e24cef70c61344ea92684c16c02ba576c9c
Vasudev Kamath <kamathvasudev@gmail.com>
2014-06-01 18:58:58 +0530

    Use custom configuration file for testing
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- tests/api/__init__.py
+++ tests/api/__init__.py
@@ -1,11 +1,14 @@
 from silpa.api import create_app
 from .. import SILPAAppTestCase, settings
+import os
 
 
 class SILPAApiTestCase(SILPAAppTestCase):
 
     def _create_app(self):
-        return create_app(settings)
+        self.conffile = os.path.join(os.path.dirname(__file__), '../resources',
+                                     'silpa.conf')
+        return create_app(self.conffile, settings)
 
     def setUp(self):
         super(SILPAApiTestCase, self).setUp()
--- tests/frontend/__init__.py
+++ tests/frontend/__init__.py
@@ -8,12 +8,15 @@
 
 from silpa.frontend import create_app
 from .. import SILPAAppTestCase, settings
+import os
 
 
 class SILPAFrontEndTestCase(SILPAAppTestCase):
 
     def _create_app(self):
-        return create_app(settings)
+        self.conffile = os.path.join(os.path.dirname(__file__), '../resources',
+                                     'silpa.conf')
+        return create_app(self.conffile, settings)
 
     def setUp(self):
         super(SILPAFrontEndTestCase, self).setUp()
