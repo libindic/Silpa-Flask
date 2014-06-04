194328253721d55af6d3d344fe54dc9128a6f224
Vasudev Kamath <kamathvasudev@gmail.com>
2014-06-04 20:32:17 +0530

    Improve config testing
    * Use assertRaises as context manager instead of doing work around to
      manually, calling init
    * Add option and section to testscenarios and check it on raised exception
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- silpa/tests/test_config.py
+++ silpa/tests/test_config.py
@@ -18,25 +18,32 @@ resource_path = partial(os.path.join, os.path.dirname(__file__), 'resources')
 class TestConfigParser(testscenarios.TestWithScenarios):
     scenarios = [
         ('main:site Missing Conf',
-         dict(location=resource_path('silpa_main_site.conf'))),
+         dict(location=resource_path('silpa_main_site.conf'),
+              section='main', option='site')),
         ('main:baseurl Missing Conf',
-         dict(location=resource_path('silpa_main_baseurl.conf'))),
+         dict(location=resource_path('silpa_main_baseurl.conf'),
+              section='main', option='baseurl')),
         ('logging:log_level Missing Conf',
-         dict(location=resource_path('silpa_logging_loglevel.conf'))),
+         dict(location=resource_path('silpa_logging_loglevel.conf'),
+              section='logging', option='log_level')),
         ('logging:log_folder Missing Conf',
-         dict(location=resource_path('silpa_logging_logfolder.conf'))),
+         dict(location=resource_path('silpa_logging_logfolder.conf'),
+              section='logging', option='log_folder')),
         ('logging:log_name Missing Conf',
-         dict(location=resource_path('silpa_logging_logname.conf'))),
+         dict(location=resource_path('silpa_logging_logname.conf'),
+              section='logging', option='log_name')),
         ('modules Missing Conf',
-         dict(location=resource_path('silpa_modules.conf'))),
+         dict(location=resource_path('silpa_modules.conf'),
+              section='modules', option=None)),
         ('modules_display Missing Conf',
-         dict(location=resource_path('silpa_module_display.conf')))
+         dict(location=resource_path('silpa_module_display.conf'),
+              section='module_display', option=None))
     ]
 
-    def setUp(self):
-        # No other better way to test
-        self.config = Config()
-
     def test_config_error_handling(self):
-        self.assertRaises(IncompleteConfigError,
-                          self.config.__init__(self.location))
+        with self.assertRaises(IncompleteConfigError) as ic:
+            Config(self.location)
+
+        e = ic.exception
+        self.assertEqual(self.option, e.option)
+        self.assertEqual(self.section, e.section)
